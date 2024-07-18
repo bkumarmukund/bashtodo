04c3094db2e53a78a69e9d7f3c3c70c5f2e072bd
Balmukund Kumar <bkumarmukund@gmail.com>
2024-07-18 20:15:36 +0530

    added rofi for interactive menu

--- bashtodo.sh
+++ bashtodo.sh
@@ -1,88 +1,68 @@
 #!/bin/bash
 
-# Set the filename for your todo list
-filename="$HOME/Documents/studyLog/$(date +%Y/%b/day%d.todo)"
+TODO_FILE="tasks.txt"
 
-# Set the notification command to be used
-  # Use notify-send
-  notify="notify-send -t 3000 -u"
-  # Use echo
-  #notify="echo "
-
-# Check if the todo file exists; if not, create it
-if [ ! -e "$filename" ]; then
-    touch "$filename"
-fi
-
-# Function to display the todo list
-display_todo() {
-  if [ -s "$filename" ]; then
-    awk -F ' ' '{ printf "[%02d] %s\n", NR, $0 }' "$filename"
-  else
-    $notify critical "No tasks found."
-  fi
-}
-
-# Function to add a task to the todo list
-add_task() {
-    echo "$1" >> "$filename"
-    $notify low "Task added: $1"
-}
-
-# Function to mark a task as complete
-complete_task() {
-    task_number="$1"
-    sed -i "${task_number}s/^/- [x] /" "$filename"
-    $notify critical "Task $task_number marked as complete."
-}
-
-# Function to delete a task from list
-delete_task() {
-  task_number="$1"
-  if [ -s "$filename" ]; then
-    if [ "$task_number" -le "$(wc -l < "$filename")" ]; then
-      sed -i "${task_number}d" "$filename"
-      $notify low "Task $task_number deleted."
-    else
-      $notify critical "Invalid task number."
+# Function to add a task
+function add_task() {
+    task=$(rofi -dmenu -p "Enter task:")
+    if [[ -n "$task" ]]; then
+        echo "[ ] $task" >> "$TODO_FILE"
+        rofi -e "Task added: $task"
     fi
-  else
-    $notify critical "No tasks found."
-  fi
 }
 
-# Parse command-line options
-while getopts ":la:c:d:" opt; do
-  case $opt in
-    l)
-      # List tasks
-      display_todo
-      exit
-      ;;
-    a)
-      # Add a task
-      add_task "$OPTARG"
-      exit
-      ;;
-    c)
-      # Complete a task
-      complete_task "$OPTARG"
-      exit
-      ;;
-    d)
-      # Delete a task
-      delete_task "$OPTARG"      
-      exit
-      ;;
-    *)
-    echo "Usage: $0 [-l] [-a task] [-c task_number] [-d task_number]"
-    ;;
-  esac
+# Function to list tasks
+function list_tasks() {
+    tasks=$(cat "$TODO_FILE" | rofi -dmenu -i -p "Tasks:")
+    echo "$tasks"
+}
+
+# Function to mark a task as done
+function complete_task() {
+    tasks=$(cat "$TODO_FILE")
+    task=$(echo "$tasks" | rofi -dmenu -i -p "Complete task:")
+    
+    if [[ -n "$task" ]]; then
+        # Extract the actual task without the status marker
+        task_content=$(echo "$task" | sed 's/^\[.\] //')
+        # Mark the task as completed in the file
+        sed -i "s/^\[ \] $task_content$/[x] $task_content/" "$TODO_FILE"
+        rofi -e "Task completed: $task_content"
+    fi
+}
+
+# Main loop for menu options
+while true; do
+    option=$(rofi -dmenu -p "Select an option:" <<EOF
+Add Task
+List Tasks
+Complete Task
+Quit
+EOF
+    )
+
+    # Exit script if Escape key is pressed
+    if [[ -z "$option" ]]; then
+        break
+    fi
+
+    case $option in
+        "Add Task")
+            add_task
+            ;;
+        "List Tasks")
+            list_tasks
+            ;;
+        "Complete Task")
+            complete_task
+            ;;
+        "Quit")
+            break
+            ;;
+        *)
+            rofi -e "Invalid option: $option"
+            ;;
+    esac
 done
 
-# Main menu
-if [ $OPTIND -eq 1 ]; then
-  # Display todo if no argument
-  display_todo
-  exit 1
-fi
+exit 0
